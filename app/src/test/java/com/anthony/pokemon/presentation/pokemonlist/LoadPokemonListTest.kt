4f61a98f1f8b48de534b88eaade9890259b32dc0
Jov Mit <jovmit@Jovs-MacBook-Pro-3.local>
2025-05-27 18:37:30 +0200

    Loading an empty list of pokemons

--- app/src/test/java/com/anthony/pokemon/presentation/pokemonlist/LoadPokemonListTest.kt
+++ app/src/test/java/com/anthony/pokemon/presentation/pokemonlist/LoadPokemonListTest.kt
@@ -1,5 +1,7 @@
 package com.anthony.pokemon.presentation.pokemonlist
 
+import com.anthony.pokemon.CoroutineTestExtension
+import com.anthony.pokemon.R
 import com.anthony.pokemon.domain.model.Pokemon
 import com.anthony.pokemon.domain.model.PokemonAbility
 import com.anthony.pokemon.domain.model.PokemonDetailData
@@ -7,10 +9,17 @@ import com.anthony.pokemon.domain.repository.PokemonRepository
 import com.anthony.pokemon.domain.usecases.GetPokemonListUseCase
 import com.anthony.pokemon.domain.util.DataError
 import com.anthony.pokemon.domain.util.Result
+import com.anthony.pokemon.presentation.common.UiText
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(CoroutineTestExtension::class)
 class LoadPokemonListTest {
 
     @Test
@@ -22,12 +31,38 @@ class LoadPokemonListTest {
         assertEquals(PokemonListUiState(), viewModel.state.value)
     }
 
+    @Test
+    fun initialPokemonLoad() = runTest {
+        val repository = FakeRepository()
+        val useCase = GetPokemonListUseCase(repository)
+        val viewModel = PokemonListViewModel(useCase)
+
+        val actualDeliveredStates = mutableListOf<PokemonListUiState>()
+        val job = launch(Dispatchers.Unconfined) {
+            viewModel.state.collect { item ->
+                println("Current: $item")
+                actualDeliveredStates.add(item)
+            }
+        }
+        job.cancel()
+
+        val expectedDeliveredStates = listOf(
+            PokemonListUiState(),
+            PokemonListUiState(
+                loading = false,
+                loadingMessage = UiText.StaticText(R.string.loading_pokemon)
+            )
+        )
+        assertEquals(expectedDeliveredStates, actualDeliveredStates)
+    }
+
     class FakeRepository: PokemonRepository {
         override fun getPokemonList(
             limit: Int,
             offset: Int
         ): Flow<Result<List<Pokemon>, DataError.Network>> {
-            TODO("Not yet implemented")
+            val data = emptyList<Pokemon>()
+            return flow { emit(Result.Success(data)) }
         }
 
         override fun getPokemonDetails(name: String): Flow<Result<PokemonDetailData, DataError.Network>> {
